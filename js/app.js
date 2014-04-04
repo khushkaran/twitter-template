6a7f9583a50d11c73d89eb330adbef6dad8a929c
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-04 19:20:16 +0100

    Images now change on hover

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
-var sourceSwap = function () {
-    var $this = $(this);
-    var newSource = $this.data('hover-src');
-    $this.data('hover-src', $this.attr('src'));
-    $this.attr('src', newSource);
-}
-
 $(function () {
-  // $('#left_menu a').hover(sourceSwap, sourceSwap);
-  $('#left_menu a').hover(function() {
-    var $this = $(this).find("img")[0];
-    var newSource = $this.data('hover-src');
-    $this.data('hover-src', $this.attr('src'));
-    $this.attr('src', newSource);
+
+  $('header a').hover(function(){
+    var newSrc = $(this).children().data("hover-src");
+    $(this).children().attr("src", newSrc);
+  },function(){
+    var newSrc = $(this).children().data("src");
+    $(this).children().attr("src", newSrc);
   });
 });
