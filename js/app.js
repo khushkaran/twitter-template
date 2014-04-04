66d0577dc34b8257c9729831a58cdb12185c8170
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-04 19:42:36 +0100

    Fixed input colouring

--- js/app.js
+++ js/app.js
@@ -7,4 +7,13 @@ $(function () {
     var newSrc = $(this).children().data("src");
     $(this).children().attr("src", newSrc);
   });
+
+  $('#search_box').focus(function(){
+    $(this).css('background-color','#66757F')
+    $(this).css('color','#A3A9AD')
+  });
+  $('#search_box').blur(function(){
+    $(this).css('background-color','#A3ACB2')
+    $(this).css('color','#D7DBDD')
+  });
 });
